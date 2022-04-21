5ccdd1b3bd270e55c83a97761f1efc12be3002a3
giuliopie <giuliopiepoli.dev@gmail.com>
2022-04-21 18:00:46 +0200

    Set attributes for Article Model

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -8,4 +8,23 @@ use Illuminate\Database\Eloquent\Model;
 class Article extends Model
 {
     use HasFactory;
+
+        /**
+     * The primary key associated with the table.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'id';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'Subject',
+        'Body',
+        'Tags',
+        'Attachment'
+    ];
 }
